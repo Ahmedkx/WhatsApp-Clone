c8cb7c64ec8e84007da10df5c8b9caba4664a7a0
Ahmedkx <midots202@gmail.com>
2022-12-09 16:20:28 +0200

    -

--- src/Components/Sidebar/Topbar.tsx
+++ src/Components/Sidebar/Topbar.tsx
@@ -1,4 +1,8 @@
-import { Stack } from '@mui/material'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Stack } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import AvatarIcon from '../../Images/AvatarIcon';
 import NewChatIcon from '../../Images/NewChatIcon';
@@ -11,19 +15,57 @@ const classes = {
 }
 
 export default function Topbar() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <Stack sx={classes.stack} flexDirection="row" justifyContent="space-between" alignItems="center" 
-            // bgcolor="rgb(240,242,245)"
+            bgcolor="rgb(240,242,245)"
             >
                     <AvatarIcon width="40px" height="40px" />
             <Stack flexDirection="row" gap="10px">
                 <IconButton sx={{padding:"8px"}}>
                     <NewChatIcon />
                 </IconButton>
-                <IconButton sx={{padding:"8px"}}>
+                <IconButton 
+                    sx={{padding:"8px"}}
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    
+                >
                     <DotsIcon />
                 </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                    }}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                >
+                    <MenuItem onClick={handleClose}>New Group</MenuItem>
+                    <MenuItem onClick={handleClose}>Settings</MenuItem>
+                    <MenuItem onClick={handleClose}>Log out</MenuItem>
+                </Menu>
             </Stack>
         </Stack>
-    )
+    );
 }
